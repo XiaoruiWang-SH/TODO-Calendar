e2fd57cd7606349bd3db33b61ce31c64354cb0e0
Xiaorui Wang <105865593+XiaoruiWang-SH@users.noreply.github.com>
2025-03-13 21:53:55 +0100

    Import tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
  * @Email: xiaorui.wang@usi.ch
  * @Date: 2025-03-13 10:48:47
  * @LastEditors: Xiaorui Wang
- * @LastEditTime: 2025-03-13 12:05:12
+ * @LastEditTime: 2025-03-13 21:02:48
  * @Description: 
  * 
  * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
@@ -12,7 +12,7 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import App from './components/app/App.js'
 
 const rootElement = document.getElementById('root');
--- src/types/img.d.ts
+++ src/types/img.d.ts
@@ -3,10 +3,11 @@
  * @Email: xiaorui.wang@usi.ch
  * @Date: 2025-03-13 10:48:47
  * @LastEditors: Xiaorui Wang
- * @LastEditTime: 2025-03-13 12:05:44
+ * @LastEditTime: 2025-03-13 21:03:14
  * @Description: 
  * 
  * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
  */
 
 declare module '*.svg';
+// declare module '*.css';
