cd823d7a22c7a36cf14fa1bac04f8a1566aef9eb
Xiaorui Wang <105865593+XiaoruiWang-SH@users.noreply.github.com>
2025-03-15 11:49:02 +0100

    Add global setting

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,7 +3,7 @@
  * @Email: xiaorui.wang@usi.ch
  * @Date: 2025-03-13 10:48:47
  * @LastEditors: Xiaorui Wang
- * @LastEditTime: 2025-03-13 16:49:04
+ * @LastEditTime: 2025-03-15 11:46:18
  * @Description: 
  * 
  * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
@@ -20,8 +20,10 @@ function App() {
   return (
       <NormalTasksProvider>
         <CompletedTasksProvider>
-          <Header />
-          <Container />
+          <div className='font-sans text-gray-900 font-medium'>
+            <Header />
+            <Container />
+          </div>
         </CompletedTasksProvider>
       </NormalTasksProvider>
   )
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,18 +3,18 @@
  * @Email: xiaorui.wang@usi.ch
  * @Date: 2025-03-13 10:48:47
  * @LastEditors: Xiaorui Wang
- * @LastEditTime: 2025-03-13 16:48:45
+ * @LastEditTime: 2025-03-15 11:45:54
  * @Description: 
  * 
  * Copyright (c) 2025 by Xiaorui Wang, All Rights Reserved. 
  */
 
-import './Header.css';
+// import './Header.css';
 import { FC } from 'react';
 
 export const Header: FC = () => {
   return (
-    <div className='header'>
+    <div className='text-3xl font-bold text-start'>
       <h1>ToDo List</h1>
     </div>
   );
